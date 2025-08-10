0c773c7f1465be02264308cb782243bc19a64c2c
robin4094 <hwangrb0310@gmail.com>
2025-08-09 21:19:14 -0700

    change3

--- main.js
+++ main.js
@@ -10,9 +10,25 @@ playerPosition = { x: 75, y: 305 };
 
 const backGroundImage = new Image();
 backGroundImage.src = "./background.png"
+const playerUpImage = new Image();
+playerUpImage.src = "./up_all.png"
+const playerDownImage = new Image();
+playerDownImage.src = "./down_all.png"
+const playerLeftImage = new Image();
+playerLeftImage.src = "./left_all.png"
+const playerRightImage = new Image();
+playerRightImage.src = "./right_all.png";
+const keys = {
+    up: { pressed: false },
+    left: { pressed: false },
+    down: { pressed: false },
+    right: { pressed: false }
+};
+
+
 
 class Sprite {
-    constructor({ 
+    constructor({
         position,
         image,
         scale = 1,
@@ -63,12 +79,111 @@ class Sprite {
 }
 
 const background = new Sprite({
-    position: { x: offset.x, y: offset.y}, 
+    position: { x: offset.x, y: offset.y },
     image: backGroundImage
-}); 
-console.log(background.position)
+});
+
+const player = new Sprite({
+    position: { x: canvas.width / 2 - 40, y: canvas.height / 2 - 40 },
+    image: playerDownImage,
+    frames: { max: 4, hold: 20 },
+    sprites: { up: playerUpImage, left: playerLeftImage, down: playerDownImage, right: playerRightImage }
+
+})
+
 function animate() {
     const animationId = window.requestAnimationFrame(animate);
     background.draw();
+    player.draw();
+    let moving = true;
+    player.animate = false;
+    if (keys.up.pressed) {
+        player.animate = true;
+        player.image = player.sprites.up;
+    }
+    if(keys.down.pressed) {
+        player.animate = false;
+        player.image = player.sprites.down;
+    }
+    if(keys.right.pressed) {
+        player.animate = false;
+        player.image = player.sprites.right;
+    }
+    if(keys.left.pressed) {
+        player.animate = false;
+        player.image = player.sprites.left;
+    }
 }
+addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case "w":
+            keys.up.pressed = true;
+            break;
+        case "a":
+            keys.left.pressed = true;
+            break;
+        case "s":
+            keys.down.pressed = true;
+            break;
+        case "d":
+            keys.right.pressed = true;
+            break;
+    }
+    // if (event.key === 'w') keys.up.pressed = true;
+    // else if (event.key ==='a') keys.left.pressed = true
+    // else if (event.key ==='s') keys.down.pressed = true
+    // else if (event.key ==='d') keys.right.pressed = true
+});
+addEventListener('keyup', (event) => {
+    switch (event.key) {
+        case "w":
+            keys.up.pressed = false;
+            break;
+        case "a":
+            keys.left.pressed = false;
+            break;
+        case "s":
+            keys.down.pressed = false;
+            break;
+        case "d":
+            keys.right.pressed = false;
+            break;
+    }
+});
+addEventListener('keyright', (event) => {
+    switch (event.key) {
+        case "w":
+            keys.up.pressed = false;
+            break;
+        case "a":
+            keys.left.pressed = false;
+            break;
+        case "s":
+            keys.down.pressed = false;
+            break;
+        case "d":
+            keys.right.pressed = false;
+            break;
+    }
+});
+addEventListener('keyleft', (event) => {
+    switch (event.key) {
+        case "w":
+            keys.up.pressed = false;
+            break;
+        case "a":
+            keys.left.pressed = false;
+            break;
+        case "s":
+            keys.down.pressed = false;
+            break;
+        case "d":
+            keys.right.pressed = false;
+            break;
+    }
+});
+
+
+
 animate();
+
